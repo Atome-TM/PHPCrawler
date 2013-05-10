905fdb666766a3187b94218050747b34ccffc01e
Thomas <thomas.moreira.ds@gmail.com>
2013-05-10 09:14:19 +0200

    Add comments (test)

--- main.php
+++ main.php
@@ -6,5 +6,5 @@ $website = "http://www.micheledighoffer.fr/"; // Website to crawl
 
 $outputTab = getTabLinks($website); // Main function crawler
 
-outputCrawl($outputTab, "xml"); // Get and View XML sitemap
+//outputCrawl($outputTab, "xml"); // Get and View XML sitemap
 ?>
