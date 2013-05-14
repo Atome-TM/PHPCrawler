2893887efa6c659069e15a719c386ba13a1126e0
Michel EDIGHOFFER <edimitchel@gmail.com>
2013-05-14 19:41:53 +0200

    Modification: PHPCrawler POO

--- crawler.php
+++ crawler.php
@@ -1,100 +1,120 @@
-<?php
-/*
- *  Function getTabLinks
- *  $site : String - Page to crawl
- *  $options : Array - Link to begin all links
- *  Return an array of page's links
- */
-function getTabLinks($site)
-{
-	//On récupère le contenu HTML du lien
-	$file = file_get_contents($site);
-	$tab_links = array();
+<?php 
 
-	$pattern = '/href="(.*)"/U'; // Pattern de recherche d'un lien entourer par href=""
-	$retour = preg_match_all($pattern, $file, $matches, PREG_PATTERN_ORDER);
-	
-	foreach($matches[1] as $key => $value)
+class PHPCrawler
+{
+	private $format;		// Format of output
+	private $tab_links;		// Array of initial links
+	private $tablinks;		// Array of all links
+	private $site;			// Url of the racine
+	private $output;		// Content of output
+
+	function __construct($site)
 	{
-		if(!preg_match('/(\.js|javascript.*|\.jpg|\.png|.gif|\.css|#.*)$/', $value)) //On ne recupère que les liens n'ayant pas d'extension
-		{
-			if(strpos($value, "http://") !== false OR strpos($value, "https://") !== false)
-			{
-				if(strpos($value, $site) !== false)
-				{
-					$tab_links[] = $value;
-				}
-			}
-			else
-			{
-				if(strlen($value) > 0)
-				{
-					$tab_links[] = $site.$value;
-				}
-			}
-		}
-		//$where = $where + $matches[1][1];
+		$this->format = "xml";		// Default format : XML
+		$this->tab_links = array();	
+		$this->getTabLinks();
+		$this->site = $site;
 	}
-	$tab_links = array_unique($tab_links);
-	return $tab_links;
-}
 
-/*
- *  Function crawler
- *  $start : Link of first link to crawl
- *  Return an array of all website's links (using getTabLinks)
- */
-function crawler($start, $options  = array())
-{
-	//Création du tableau de départ
-	$tablinks = getTabLinks($start, $options);
-
-	//Boucle tant qu'au moins un nouveau lien a été trouvé, on rescanne le tableau
-	$hasNewLink = true;
-	while($hasNewLink)
+	/*
+	 *  Function getTabLinks
+	 *  $site : String - Page to crawl
+	 *  $options : Array - Link to begin all links
+	 *  Return an array of page's links
+	 */
+	function getTabLinks($site = false, $options = array())
 	{
-	  $hasNewLink = false;
-		foreach($tablinks as $link)
+		//On récupère le contenu HTML du lien
+		if($site === false)
+			$file = file_get_contents($this->site);
+		else
+			$file = file_get_contents($site);
+
+		$pattern = '/href="(.*)"/U'; // Pattern de recherche d'un lien entourer par href=""
+		$retour = preg_match_all($pattern, $file, $matches, PREG_PATTERN_ORDER);
+		
+		foreach($matches[1] as $key => $value)
 		{
-			// On execute la fonction sur chaque lien du tableau et on verifie si chaque lien retourné existe deja ou non
-			foreach(getTabLinks($link, $options) as $newlink)
+			if(!preg_match('/(\.js|javascript.*|\.jpg|\.png|.gif|\.css|#.*)$/', $value)) //On ne recupère que les liens n'ayant pas d'extension
 			{
-				if(!in_array($newlink, $tablinks))
+				if(strpos($value, "http://") !== false OR strpos($value, "https://") !== false)
 				{
-					$hasNewLink = true;
-					$tablinks[] = $newlink;
+					if(strpos($value, $site) !== false)
+					{
+						$this->tab_links[] = $value;
+					}
+				}
+				else
+				{
+					if(strlen($value) > 0)
+					{
+						$this->tab_links[] = $this->site.$value;
+					}
+				}
+			}
+			//$where = $where + $matches[1][1];
+		}
+		$this->tab_links = array_unique($tab_links);
+		return $this->tab_links;
+	}
+
+	/*
+	 *  Function crawler
+	 *  $start : Link of first link to crawl
+	 *  Return an array of all website's links (using getTabLinks)
+	 */
+	function crawler($start, $options  = array())
+	{
+		//Création du tableau de départ
+		$this->tablinks = $this->getTabLinks($start, $options);
+
+		//Boucle tant qu'au moins un nouveau lien a été trouvé, on rescanne le tableau
+		$hasNewLink = true;
+		while($hasNewLink)
+		{
+			$hasNewLink = false;
+			foreach($this->tablinks as $link)
+			{
+				// On execute la fonction sur chaque lien du tableau et on verifie si chaque lien retourné existe deja ou non
+				foreach($this->getTabLinks($link, $options) as $newlink)
+				{
+					if(!in_array($newlink, $this->tablinks))
+					{
+						$hasNewLink = true;
+						$this->tablinks[] = $newlink;
+					}
 				}
 			}
 		}
+		$this->tablinks[] = $start;
+		sort($this->tablinks); // Trie du tableau finale
+		return $this->tablinks;
 	}
-	$tablinks[] = $start;
-	sort($tablinks); // Trie du tableau finale
-	return $tablinks;
-}
 
-/*
- *  Function outputCrawl
- *  $tablinks : Array - Array return by crawler() function
- *  $format : String - Format of output (only xml for now)
- *  Print the result in $format
- */
-function outputCrawl($tablinks, $format = "xml")
-{
-	if($format == "xml")
+	/*
+	 *  Function outputCrawl
+	 *  $tablinks : Array - Array return by crawler() function
+	 *  $format : String - Format of output (only xml for now)
+	 *  Print the result in $format
+	 */
+	function outputCrawl()
 	{
-		$output = '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
-		if(count($tablinks) > 0) {
-			foreach($tablinks AS $link)
-			{
-				$output .="<url>";
-				$output .= '<loc>'.$link.'</loc>';
-				$output .="</url>";
+		if($this->format == "xml")
+		{
+			$this->output = '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+			if(count($this->tablinks) > 0) {
+				foreach($this->tablinks AS $link)
+				{
+					$this->output .="<url>";
+					$this->output .= '<loc>'.$link.'</loc>';
+					$this->output .="</url>";
+				}
 			}
-		}
-		$output .="</urlset>";
+			$this->output .="</urlset>";
 
-		header('Content-Type: text/xml');
-		echo $output;
+			header('Content-Type: text/xml');
+			echo $this->output;
+		}
 	}
 }
 ?>
