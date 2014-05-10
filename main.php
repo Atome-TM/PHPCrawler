6540bd4dfe3fa4fd697daa4a9a8f1e74d0f928ab
Thomas Moreira <thomasmds@users.noreply.github.com>
2014-05-10 12:08:18 +0200

    change example

--- main.php
+++ main.php
@@ -2,7 +2,7 @@
 //Site à crawler
 require_once("crawler.php");
 
-$website = "http://www.thomasmoreira.com"; // Website to crawl
+$website = "http://www.thomasmoreira.fr"; // Website to crawl
 
 $outputTab = new PHPCrawler($website); // Create a new class PHPCrawler
 $outputTab->outputCrawl(); // Get and Generate the XML sitemap
