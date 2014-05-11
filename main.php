3656732e390027cfe52d00fdf90928091c3a85a4
Thomas Moreira <thomas.moreira.ds@gmail.com>
2014-05-11 13:32:16 +0200

    Add some example to use it

--- main.php
+++ main.php
@@ -5,5 +5,13 @@ require_once("crawler.php");
 $website = "http://www.thomasmoreira.fr"; // Website to crawl
 
 $outputTab = new PHPCrawler($website); // Create a new class PHPCrawler
+
+// Example 1 : Output inline XML format (By default 'xml', 'I')
+$outputTab->outputCrawl();
+
+// Example 2 : Return a PHP array
+$return = $outputTab->outputCrawl("array");
+
+// Example 3 : Ceate and redirect to a Sitempa.xml file
 $outputTab->outputCrawl("xml", "F"); // Get and Generate the XML sitemap
 ?>
